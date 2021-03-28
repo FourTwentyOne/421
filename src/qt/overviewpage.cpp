5475f9a9313cbd104801c17ae885ffe7410a3dc0
nerdlabs001 <nerdlabs001@gmail.com>
2021-03-28 17:45:14 -0500

    overview re-frame

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -12,7 +12,7 @@
 #include <QAbstractItemDelegate>
 #include <QPainter>
 
-#define DECORATION_SIZE 50
+#define DECORATION_SIZE 85
 #define NUM_ITEMS 5
 
 class TxViewDelegate : public QAbstractItemDelegate
