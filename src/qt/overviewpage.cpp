1154d1a5be30d4231405401a2275506b8ded0bb5
nerdlabs001 <nerdlabs001@gmail.com>
2021-03-28 20:53:45 -0500

    swap info button

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -12,8 +12,8 @@
 #include <QAbstractItemDelegate>
 #include <QPainter>
 
-#define DECORATION_SIZE 85
-#define NUM_ITEMS 5
+#define DECORATION_SIZE 64
+#define NUM_ITEMS 1
 
 class TxViewDelegate : public QAbstractItemDelegate
 {
